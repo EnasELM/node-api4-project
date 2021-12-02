c7eb7b8cffac8ec52bcf8f8005be34493ed87366
Enas <enaselmahdwi@gmail.com>
2021-12-02 14:23:05 -0700

    wip

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors')
 
 const server = express();
+server.use(express.json())
+server.use(cors())
+
 server.get('/api/users', (req, res) => {
-    res.json([{id:1, username:'foo',},
-    {id:2, username:'foo1',},
-    {id:3, username:'foo2',}])
+    res.json({message: 'api is here!!'})
 })
 
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      message: `Horror in the router: ${err.message}`,
+      stack: err.stack,
+    })
+  })
+  
 const port = process.env.PORT || 9000;
 
 
